d4f1fe17dd1327648e4c7c177ca2c84806262680
Maxim <maxim@volleythat.com>
2020-07-29 11:43:59 +0900

    sending a sample output

--- index.js
+++ index.js
@@ -13,5 +13,27 @@ exports.handler = async (event) => {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
     };
-    return response;
+    let output =  {
+        "version": "1.0",
+            "sessionAttributes": {},
+            "response": {
+                "outputSpeech": {
+                    "type": "SSML",
+                    "ssml": "Hi there!",
+                },
+                "card": {
+                    "type": "Simple",
+                    "title": "Hello There! skill",
+                    "content": "A skill that always says hi."
+                },
+                "reprompt": {
+                    "outputSpeech": {
+                      "type": "SSML",
+                      "ssml": "Won't need a reprompt for now."
+                    }
+                },
+                "shouldEndSession": true
+            }
+        };
+    return output;
 };
