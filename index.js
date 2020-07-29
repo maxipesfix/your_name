d8c7a3df10d4a8a5c275828823e84674c70f7c6e
Maxim <maxim@volleythat.com>
2020-07-29 11:21:57 +0900

    printing input

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
 */
 
 exports.handler = async (event) => {
-    // TODO implement
+    
+
+    console.log("Input event: " + JSON.stringify(event));
+    
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
