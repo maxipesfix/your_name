5ed65069e1219c06cf20013baeab2ce2ce9dc120
Maxim For Demos <demo@Maxims-MacBook-Pro-2.local>
2020-07-29 10:19:52 +0900

    lambda sample

--- index.js
+++ index.js
@@ -4,4 +4,11 @@
  
 */
 
-'use strict';
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
